acf5c14b17fb2ced11e094b0a15115ab70c64b67
bruno foggia <x@bruno.pro>
2023-05-29 11:20:03 -0300

    refactor: rename fn

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,17 +3,17 @@ import { throwHttpException } from './errors';
 
 export const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
 
-const parallelizeDefaults = { checkInterval: 100, timeout: 60000, step: 0, async: 1 };
-export const parallelize =
+const queuelizeDefaults = { checkInterval: 100, timeout: 60000, step: 0, async: 1 };
+export const queuelize =
     async (condition, _execute, config: any = {}) => {
-        config = defaults(config, parallelizeDefaults);
+        config = defaults(config, queuelizeDefaults);
         const options = defaults(config._options || {}, { parts: 0, finished: 0, timeSpent: 0 });
         const finish = async () => {
             if (options.finished === options.parts) return true;
             await sleep(config.checkInterval);
             options.timeSpent += config.checkInterval;
             if (config.timeout && options.timeSpent >= config.timeout) {
-                return throwHttpException('PARALLELIZE_TIMEOUT');
+                return throwHttpException('QUEUELIZE_TIMEOUT');
             }
             return await finish();
         };
