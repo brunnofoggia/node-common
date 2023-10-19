0b5585d89c4856ade9a9df211d5109ef37bb393c
bruno foggia <brunnofoggia@gmail.com>
2023-10-19 15:51:41 -0300

    feat(process util): added options to get unformated output

--- src/utils/process.ts
+++ src/utils/process.ts
@@ -6,32 +6,32 @@ export class ProcessUtil {
     protected static _instance: ProcessUtil;
 
     static instance() {
-        if (!this._instance) this._instance = new ProcessUtil;
+        if (!this._instance) this._instance = new ProcessUtil();
         return this._instance;
     }
 
     time(name) {
-        return this.times[name] = Date.now();
+        return (this.times[name] = Date.now());
     }
 
-    timeEnd(name) {
+    timeEnd(name, format = true) {
         if (!this.times[name]) {
             debug(`There is no time with the name ${name}`);
             return;
         }
         const start = this.times[name];
         const end = Date.now();
-        const elapsedTime = this.formatElapsedTime(end - start);
+        const elapsedTime = end - start;
 
         this.times[name] = null;
-        return elapsedTime;
+        return format ? this.formatElapsedTime(elapsedTime) : elapsedTime;
     }
 
     formatElapsedTime(elapsedTime) {
         const minutes = Math.floor(elapsedTime / 60000);
-        const remainingTime = elapsedTime - (minutes * 60000);
+        const remainingTime = elapsedTime - minutes * 60000;
         const seconds = Math.floor(remainingTime / 1000);
-        const ms = remainingTime - (seconds * 1000);
+        const ms = remainingTime - seconds * 1000;
 
         const result = [];
         if (minutes > 0) {
