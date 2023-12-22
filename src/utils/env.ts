2a90c537fa4fe94acd4ce3210d66438866264a65
bruno foggia <brunnofoggia@gmail.com>
2023-12-21 21:45:16 -0300

    feat(env): prefix optional

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -5,7 +5,12 @@ export const setEnvPrefix = (_prefix) => {
 };
 
 export const env = (key, _default = '', group = 'DEFAULT') => {
-    const path: string = [prefix, group.toUpperCase(), key].join('_');
+    const values = [];
+    if (prefix) values.push(prefix);
+    if (group) values.push(group); // .toUpperCase()
+    values.push(key);
+
+    const path: string = values.join('_');
     const value = process.env[path] || _default;
 
     return value;
